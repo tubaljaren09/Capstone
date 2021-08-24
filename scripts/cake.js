20e05746f817b753acc3badb9c91c8e854b5c3af
Jaren <tubaljaren09@gmail.com>
2021-08-24 15:47:36 +0800

    Add video script on cake

--- scripts/cake.js
+++ scripts/cake.js
@@ -1,11 +1,25 @@
-let button1 = document.getElementById('button1');
-button1.addEventListener('click', () => {
-    document.querySelector('.bg-modal').style.display = "flex";
-});
-
-let close = document.querySelector('.close');
-let iframe = document.getElementById('video');
-close.addEventListener('click', () => {
-    document.querySelector('.bg-modal').style.display = "none";
-    iframe.src = iframe.src;
-});
+/* Open modal */
+let buttons = document.getElementsByTagName("button");
+for ( i = 0; i < buttons.length; i += 1) {
+    buttons[i].onclick = function() {
+        if(this.id === "btn1"){
+            document.querySelector('.bg-modal1').style.display = "flex";
+        }else if(this.id === "btn2"){
+            document.querySelector('.bg-modal2').style.display = "flex";
+        }
+    }
+} 
+/* Close modal */
+let closes = document.getElementsByClassName("close");
+let iframe = document.querySelectorAll('.video');
+for ( i = 0; i < closes.length; i += 1) {
+    closes[i].onclick = function() {
+        if(this.id === "close1"){
+            document.querySelector('.bg-modal1').style.display = "none";
+            iframe[0].src = iframe.src;
+        }else if(this.id === "close2"){
+            document.querySelector('.bg-modal2').style.display = "none";
+            iframe[1].src = iframe.src;
+        }
+    }
+}
